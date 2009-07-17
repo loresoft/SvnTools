5d6804f716bff666ab65a996641e464ed233bcb2
paul.welter <paul.welter@ac3d5786-6075-11de-bc6d-35d2ebd5fd4e>
2009-07-17 18:18:11 +0000

    - more refactoring

---
+++ Source/SvnTools.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SvnTools.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LoreSoft")]
+[assembly: AssemblyProduct("SvnTools.Tests")]
+[assembly: AssemblyCopyright("Copyright © LoreSoft 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c4beb8fd-b15d-4037-b836-668ef36792a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/SvnTools.Tests/Services/HotCopyTest.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+using SvnTools.Services;
+using SvnTools.Utility;
+
+namespace SvnTools.Tests.Services
+{
+    [TestFixture]
+    public class HotCopyTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //TODO: NUnit setup
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //TODO: NUnit TearDown
+        }
+
+        [Test]
+        public void Execute()
+        {
+            string repositoryPath = @"D:\svn\repo\Calculator";
+            string backupPath = Path.GetFullPath(@"Calculator-backup");
+
+            if (Directory.Exists(backupPath))
+                PathHelper.DeleteDirectory(backupPath);
+            
+            if (!Directory.Exists(backupPath))
+                Directory.CreateDirectory(backupPath);
+            
+            HotCopy hotcopy = new HotCopy();
+            hotcopy.RepositoryPath = repositoryPath;
+            hotcopy.BackupPath = backupPath;
+
+            bool result = hotcopy.Execute();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
